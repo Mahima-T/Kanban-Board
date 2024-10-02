efb27288ba8e275745bff6ce4368b3a97c070f56
unknown <tripathimahima53@gmail.com>
2024-10-02 23:20:57 +0530

    Updates

---
+++ precache-manifest.2423657b49e8896060a9f00a07c527af.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40e9b88bf9fbb41146e076807259fef",
+    "url": "/Kanban-Board/index.html"
+  },
+  {
+    "revision": "7fcda83d27da00f6969f",
+    "url": "/Kanban-Board/static/css/main.9d656271.chunk.css"
+  },
+  {
+    "revision": "d2bc15743d2b58cc2dce",
+    "url": "/Kanban-Board/static/js/2.cced4c53.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/Kanban-Board/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "7fcda83d27da00f6969f",
+    "url": "/Kanban-Board/static/js/main.262072c6.chunk.js"
+  },
+  {
+    "revision": "d6f5027a32c9f4544ac1",
+    "url": "/Kanban-Board/static/js/runtime~main.f8d8779d.js"
+  },
+  {
+    "revision": "6c272d9dbc2afbd2bcb03b63c12fec74",
+    "url": "/Kanban-Board/static/media/3 dot menu.6c272d9d.svg"
+  },
+  {
+    "revision": "47973b095119ca88f2cf7435ae64b951",
+    "url": "/Kanban-Board/static/media/Backlog.47973b09.svg"
+  },
+  {
+    "revision": "b96d92a33a5b1ec307931b7e2a8d7d32",
+    "url": "/Kanban-Board/static/media/Display.b96d92a3.svg"
+  },
+  {
+    "revision": "10cfcedf7c5bca2c132f8c693ea09b8d",
+    "url": "/Kanban-Board/static/media/Img - High Priority.10cfcedf.svg"
+  },
+  {
+    "revision": "2ad281d36a00948dc82df990fa6ed849",
+    "url": "/Kanban-Board/static/media/Img - Low Priority.2ad281d3.svg"
+  },
+  {
+    "revision": "861ebea52035117975c064836e6b5f25",
+    "url": "/Kanban-Board/static/media/Img - Medium Priority.861ebea5.svg"
+  },
+  {
+    "revision": "0d2cbadcaecb54771adf2fc3a4b7e6db",
+    "url": "/Kanban-Board/static/media/No-priority.0d2cbadc.svg"
+  },
+  {
+    "revision": "4eca8404cb3c7d84e8185cf924ec035f",
+    "url": "/Kanban-Board/static/media/SVG - Urgent Priority colour.4eca8404.svg"
+  },
+  {
+    "revision": "8703a249d4b8d005f438631a40938022",
+    "url": "/Kanban-Board/static/media/SVG - Urgent Priority grey.8703a249.svg"
+  },
+  {
+    "revision": "991aa8ffc2f2062e7a647ea42988b361",
+    "url": "/Kanban-Board/static/media/To-do.991aa8ff.svg"
+  },
+  {
+    "revision": "a19048c91b13f82bfcbc6917f2586ec9",
+    "url": "/Kanban-Board/static/media/add.a19048c9.svg"
+  },
+  {
+    "revision": "990a90264a79616a3efb8d5b525d4d66",
+    "url": "/Kanban-Board/static/media/down.990a9026.svg"
+  },
+  {
+    "revision": "6f7f6c8e224d8356b240a937ce34069f",
+    "url": "/Kanban-Board/static/media/in-progress.6f7f6c8e.svg"
+  },
+  {
+    "revision": "07d5a75c53e51148cac3dc84709a55f6",
+    "url": "/Kanban-Board/static/media/profile.07d5a75c.svg"
+  }
+]);
